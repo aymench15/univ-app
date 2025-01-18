63ff112ffdd37a6c7777eeff8cf71877fa597383
Aimen Chaib <62136824+aymench15@users.noreply.github.com>
2025-01-18 11:33:04 +0100

    last changesss

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
-export const BASE_URL = "http://localhost:8800/api/";
+export const BASE_URL = "https://phdexo.univ-biskra.dz/api/";
 // "https://psychare.onrender.com/api/";
 export const token = localStorage.getItem("token");
